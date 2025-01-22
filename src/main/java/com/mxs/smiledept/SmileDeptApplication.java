5134e4342453248416507964e11e7c8bad05b0f7
JeffreyMing <caojinming0929@163.com>
2025-01-22 12:46:14 +0800

    创建了项目文档

---
+++ src/main/java/com/mxs/smiledept/SmileDeptApplication.java
@@ -0,0 +1,13 @@
+package com.mxs.smiledept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmileDeptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmileDeptApplication.class, args);
+    }
+
+}
