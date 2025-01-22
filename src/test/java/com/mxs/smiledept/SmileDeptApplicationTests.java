ac730d01bb7ea3e2098987b376ad677379fe0b95
JeffreyMing <caojinming0929@163.com>
2025-01-22 12:46:14 +0800

    创建了项目文档

---
+++ src/test/java/com/mxs/smiledept/SmileDeptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mxs.smiledept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmileDeptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
